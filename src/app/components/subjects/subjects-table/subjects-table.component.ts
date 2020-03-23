20a275e63c00361531c6a7e914eae4de6c9082ae
Siarhei Ivanouski <Siarhei_Ivanouski@epam.com>
2020-03-23 10:06:37 +0300

    crlf fix

--- src/app/components/subjects/subjects-table/subjects-table.component.ts
+++ src/app/components/subjects/subjects-table/subjects-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, ValidatorFn, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Sort } from '@angular/material/sort';
 import { ActivatedRoute } from '@angular/router';
 import * as moment from 'moment';
