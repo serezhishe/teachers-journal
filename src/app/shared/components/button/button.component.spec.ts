742a7cf802a0ababde954ea79ba165155c1a6810
Siarhei Ivanouski <Siarhei_Ivanouski@epam.com>
2020-04-10 14:52:49 +0300

    trying to test routerLink on button element fix test.ts changed by tslint

--- src/app/shared/components/button/button.component.spec.ts
+++ src/app/shared/components/button/button.component.spec.ts
@@ -1,6 +1,10 @@
+import { Location } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler';
 import { Component } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TranslateMockPipe } from '../../pipes/translate-mock-pipe.pipe';
 
@@ -11,16 +15,36 @@ import { ButtonComponent } from './button.component';
 })
 class TestHostComponent {
   public type: string;
+  constructor(public readonly location: Location) {}
 }
+// tslint:disable-next-line: max-classes-per-file
+@Component({
+  template: '',
+})
+class DummyComponent {}
 
 fdescribe('ButtonComponent', () => {
   let testHost: TestHostComponent;
   let testHostFixture: ComponentFixture<TestHostComponent>;
+  let router: Router;
+  let location: Location;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ButtonComponent, TestHostComponent, TranslateMockPipe],
+      declarations: [ButtonComponent, TestHostComponent, TranslateMockPipe, DummyComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: 'form', component: DummyComponent },
+          { path: 'prefix', component: DummyComponent },
+          { path: 'prefix/test', component: TestHostComponent },
+          { path: 'prefix/test/form', component: DummyComponent },
+         ]),
+      ],
     }).compileComponents();
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    router.initialNavigation();
+    router.navigateByUrl('prefix/test');
   }));
 
   beforeEach(() => {
@@ -42,10 +66,26 @@ fdescribe('ButtonComponent', () => {
       expect(testHostFixture.nativeElement.querySelector('div button').innerText).toEqual('delete');
     });
 
-    it('add', () => {
-      testHost.type = 'add';
-      testHostFixture.detectChanges();
-      expect(testHostFixture.nativeElement.querySelector('div button').innerText).toEqual('add');
+    fdescribe('add', () => {
+      it('contain proper text', () => {
+        testHost.type = 'add';
+        testHostFixture.detectChanges();
+        expect(testHostFixture.nativeElement.querySelector('div button').innerText).toEqual('add');
+      });
+
+      it('contain rel link to form', fakeAsync(() => {
+        testHost.type = 'add';
+        testHostFixture.detectChanges();
+
+        const button = testHostFixture.debugElement.query(By.css('div button'));
+        button.nativeElement.click();
+        tick();
+
+        testHostFixture.whenStable().then(() => {
+          expect(location.path()).toBe('prefix/test/form'); // actually '/form'
+        });
+        expect(true).toBeTruthy();
+      }));
     });
 
     it('cancel', () => {
@@ -54,10 +94,17 @@ fdescribe('ButtonComponent', () => {
       expect(testHostFixture.nativeElement.querySelector('div button').innerText).toEqual('app.cancel');
     });
 
-    it('go-back', () => {
-      testHost.type = 'go-back';
-      testHostFixture.detectChanges();
-      expect(testHostFixture.nativeElement.querySelector('div button').innerText).toEqual('app.cancel');
+    fdescribe('go-back', () => {
+      it('contain proper text', () => {
+        testHost.type = 'go-back';
+        testHostFixture.detectChanges();
+        expect(testHostFixture.nativeElement.querySelector('div button').innerText).toEqual('app.cancel');
+      });
+      it('contain rel link to form', () => {
+        testHost.type = 'go-back';
+        testHostFixture.detectChanges();
+        expect(true).toBeTruthy();
+      });
     });
   });
 });
