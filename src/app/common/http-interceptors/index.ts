6dfb4f228fedf25ff0ab1cf5c2f8cda53eb3fc25
Siarhei Ivanouski <Siarhei_Ivanouski@epam.com>
2020-04-16 13:26:16 +0300

    deleted hhtp delaying used for debugging

--- src/app/common/http-interceptors/index.ts
+++ src/app/common/http-interceptors/index.ts
@@ -1,11 +1,11 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { DelayInterceptor } from './delay-interceptor';
 import { MapFixInterceptor } from './map-fix-interceptor';
+import { PopUpInterceptor } from './pop-up-interceptor';
 import { StorageInterceptor } from './storage-interceptor';
 
 export const httpInterceptorProviders = [
   { provide: HTTP_INTERCEPTORS, useClass: MapFixInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: StorageInterceptor, multi: true },
-  { provide: HTTP_INTERCEPTORS, useClass: DelayInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: PopUpInterceptor, multi: true },
 ];
--- src/app/common/http-interceptors/pop-up-interceptor.ts
+++ src/app/common/http-interceptors/pop-up-interceptor.ts
@@ -1,23 +1,16 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { catchError, delay, retry } from 'rxjs/operators';
+import { catchError, retry } from 'rxjs/operators';
 
 import { PopUpService } from '../services/pop-up.service';
 
-const HTTP_DELAY = 1000;
-
 @Injectable()
-export class DelayInterceptor implements HttpInterceptor {
+export class PopUpInterceptor implements HttpInterceptor {
   constructor(private readonly popUpService: PopUpService) {}
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (req instanceof HttpRequest) {
-      console.log(`DELAYING......${req.url}`);
-    }
-
     return req instanceof HttpRequest
       ? next.handle(req).pipe(
-          delay(HTTP_DELAY),
           retry(2),
           catchError((error: HttpErrorResponse) => {
             this.popUpService.errorMessage(error.message);
